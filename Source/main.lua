66dad840e9f6b964f397623288af7525c5d4b1ba
Nino van Hooff <nino@q42.nl>
2022-03-11 18:34:38 +0100

    ADD partial level loading

--- Source/main.lua
+++ Source/main.lua
@@ -1,3 +1,20 @@
+import "util.lua"
+import "level.lua"
+import "init.lua"
+
+function startGame()
+    LoadFile("levels/LEVEL01.CGP")
+    -- GameBR("levels/LEVEL01.CGP")
+end
+
+sucs,err = xpcall(startGame,Error_Handler)
+if sucs then
+    printf("done")
+    error("done")
+else
+    error(err)
+end
+
 local gfx = playdate.graphics
 
 gfx.setColor(gfx.kColorBlack)
