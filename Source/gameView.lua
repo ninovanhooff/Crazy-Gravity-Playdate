d2465b53d0d9a167f48ea8a7f8629d6af4b469ab
Nino van Hooff <nino@q42.nl>
2022-04-01 17:37:44 +0200

    CHANGE minor performance improvements

--- Source/gameView.lua
+++ Source/gameView.lua
@@ -15,14 +15,13 @@ function RenderBackground()
 end
 
 function RenderGame()
-    sprite:setInverted(false)
     gfx.setColor(gfx.kColorBlack)
 
     RenderBackground()
 
     for i,item in ipairs(specialT) do -- special blocks
         scrX,scrY = (item.x-camPos[1])*8-camPos[3],(item.y-camPos[2])*8-camPos[4]
-        if item.x+item.w>=camPos[1] and item.x<=camPos[1]+61 and item.y+item.h>=camPos[2] and item.y<camPos[2]+33 then
+        if item.x+item.w>=camPos[1] and item.x<=camPos[1]+gameWidthTiles+1 and item.y+item.h>=camPos[2] and item.y<camPos[2]+gameHeightTiles+1 then
             specialRenders[item.sType-7](item)
         end
     end
