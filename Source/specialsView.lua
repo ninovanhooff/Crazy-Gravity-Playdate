7e248f5cdc783b6c3243d10419f39dac109cf758
Nino van Hooff <nino@q42.nl>
2022-04-17 00:19:49 +0200

    CHANGE: improve alignment of remaining freight indicator

--- Source/specialsView.lua
+++ Source/specialsView.lua
@@ -42,7 +42,7 @@ function RenderPlatform(item)
             gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
         end
         sprite:draw(scrX+32, pltfrmY-16, unFlipped, 192, 346, 16, 16)
-        monoFont:drawText(table.sum(remainingFreight)+#planeFreight, scrX+36, pltfrmY - 12)
+        monoFont:drawText(table.sum(remainingFreight)+#planeFreight, scrX+36, pltfrmY - 11)
         gfx.setImageDrawMode(gfx.kDrawModeCopy)
 
     elseif item.pType==2 then -- freight
