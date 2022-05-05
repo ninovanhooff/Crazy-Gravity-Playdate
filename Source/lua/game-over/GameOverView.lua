66175f3ee8e85beac3b3060c8ef480978c2737d8
Nino van Hooff <nino@q42.nl>
2022-05-06 00:37:51 +0200

    CHANGE game-over logic
    And an icon fix

--- Source/lua/game-over/GameOverView.lua
+++ Source/lua/game-over/GameOverView.lua
@@ -3,9 +3,11 @@ import "CoreLibs/graphics"
 
 
 local gfx <const> = playdate.graphics
+local deSelectedDrawFun <const> = gfx.drawCircleAtPoint
+local selectedDrawFun <const> = gfx.fillCircleAtPoint
 local dialogRect <const> = playdate.geometry.rect.new(80, 50, 240, 140)
 local dialogPadding <const> = 15
-local buttonRadius <const> = 24
+local buttonRadius <const> = 28
 local buttonInsetX, buttonCenterY <const> = 80, dialogRect.bottom - dialogPadding - buttonRadius - 7
 local levelSelectCenterPoint <const> = playdate.geometry.point.new(dialogRect.x + buttonInsetX, buttonCenterY)
 local retryCenterPoint <const> = playdate.geometry.point.new(dialogRect.right - buttonInsetX, buttonCenterY)
@@ -20,17 +22,27 @@ function GameOverView:init()
 
 end
 
+local function buttonDrawFun(selected)
+    if selected then
+        return selectedDrawFun
+    else
+        return deSelectedDrawFun
+    end
+end
+
 function GameOverView:render(viewModel)
     gfx.setClipRect(dialogRect)
     gfx.clear()
     gfx.setColor(gfx.kColorBlack)
-    --gfx.setStrokeLocation(gfx.kStrokeInside)
+    gfx.setStrokeLocation(gfx.kStrokeInside)
     gfx.drawRect(dialogRect)
     gfx.setLineWidth(4)
     titleFont:drawTextAligned(viewModel.title, dialogRect:centerPoint().x, dialogRect.y+dialogPadding, kTextAlignment.center)
 
-    gfx.drawCircleAtPoint(levelSelectCenterPoint, buttonRadius)
-    gfx.drawCircleAtPoint(retryCenterPoint, buttonRadius)
+    buttonDrawFun(viewModel:isLevelSelectSelected())(levelSelectCenterPoint, buttonRadius)
+    buttonDrawFun(viewModel:isRetrySelected())(retryCenterPoint, buttonRadius)
+
+    gfx.setImageDrawMode(gfx.kDrawModeNXOR) --text color
     levelSelectIcon:draw(levelSelectCenterPoint.x-16,levelSelectCenterPoint.y-16)
     retryIcon:draw(retryCenterPoint.x-18,retryCenterPoint.y-16)
 end
--- Source/lua/game-over/GameOverViewModel.lua
+++ Source/lua/game-over/GameOverViewModel.lua
@@ -3,20 +3,45 @@ import "CoreLibs/object"
 local justPressed <const> = playdate.buttonJustPressed
 local buttonA <const> = playdate.kButtonA
 local buttonB <const> = playdate.kButtonB
+local selectButtons <const> = playdate.kButtonLeft | playdate.kButtonRight
 
 class("GameOverViewModel").extends()
 
 function GameOverViewModel:init()
     self.title = "GAME OVER!"
+    self.levelSelectSelected = false
+    self.retrySelected = true
+end
+
+function GameOverViewModel:isRetrySelected()
+    return self.retrySelected
+end
+
+function GameOverViewModel:isLevelSelectSelected()
+    return not self.retrySelected
+end
+
+local function quitLevel()
+    -- back to LevelSelect
+    popScreen() -- (this) GameOverScreen
+    popScreen() -- underlying GameScreen
+end
+
+local function retryGame()
+    -- back to game
+    popScreen()
 end
 
 function GameOverViewModel:update()
-    if justPressed(buttonA) then
-        -- back to game
-        popScreen()
+    if justPressed(selectButtons) then
+        self.retrySelected = not self.retrySelected
+    elseif justPressed(buttonA) then
+        if self.retrySelected then
+            retryGame()
+        else
+            quitLevel()
+        end
     elseif justPressed(buttonB) then
-        -- back to LevelSelect
-        popScreen() -- (this) GameOverScreen
-        popScreen() -- underlying GameScreen
+        quitLevel()
     end
 end
