54ee700cb1fb18721364a5a53e6960099b3a8730
Nino van Hooff <nino@q42.nl>
2022-10-07 17:16:19 +0200

    CHANGE pickup delay to 0.5s

--- Source/lua/specialsViewModel.lua
+++ Source/lua/specialsViewModel.lua
@@ -83,7 +83,7 @@ function CalcPlatform(item,idx)
             if Sounds then landing_sound:play() end
         end
     elseif not flying then
-        if landedTimer>frameRate and landedAt==idx then -- 1 secs and landed at cur pltfrm
+        if landedTimer>frameRate/2 and landedAt==idx then -- 1 secs and landed at cur pltfrm
             --printf(item.pType,#planeFreight,"HJ")
             if item.pType==1 and #planeFreight>0 then -- dump
                 if Sounds then
