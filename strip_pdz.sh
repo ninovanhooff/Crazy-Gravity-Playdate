6f7da4d2dcc9467d1d71c25bc6f998e46b029f8d
Nino van Hooff <nino@q42.nl>
2022-10-07 13:50:08 +0200

    BUGFIX support imports with extension

--- strip_pdz.sh
+++ strip_pdz.sh
@@ -8,10 +8,12 @@ pdxDir="$invokeDir/$pdxPath"
 cd "$sourceDir" || exit
 
 grep -r '^import' . | while read -r line ; do
-  #echo "hello $line"
+  echo "hello $line"
   matchFilePath=$(echo "$line" | sed -r 's/(.*)\:.*$/\1/')
   matchDirPath=$(echo "$matchFilePath" | sed -r 's/(.*)\/.*.lua$/\1/')
   pdzName=$(echo "$line" | sed -r 's/.*import \"(.*)\"$/\1/')
+  pdzName=$(echo "$pdzName" | sed -r 's/(.*).lua$/\1/') || "$pdzName" # strip .lua if found
+  echo "pdzName $pdzName"
   pdzPath="$invokeDir/$pdxPath/$matchDirPath/$pdzName.pdz"
   if [ -f "$pdzPath" ] ; then # file may not exist when it was deleted in the same run. imports for the same file may be found in multiple source files
       echo "DELETE $pdzPath"
