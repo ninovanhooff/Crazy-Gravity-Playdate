3ee32dd240aa02d0f937d41c8209c2e4aaf437e6
Nino van Hooff <nino@q42.nl>
2023-01-12 00:11:05 +0100

    BUGFIX support imports with returns in strip_pdz

--- strip_pdz.sh
+++ strip_pdz.sh
@@ -15,12 +15,12 @@ removeIfExists() {
 cd "$sourceDir" || exit
 
 # Find all import statements and delete the corresponding pdz files
-grep -r '^import' . | while read -r line ; do
+grep -r 'import \"' . | while read -r line ; do
   matchFilePath=$(echo "$line" | sed -r 's/(.*)\:.*$/\1/')
   matchDirPath=$(echo "$matchFilePath" | sed -r 's/(.*)\/.*.lua$/\1/')
   pdzName=$(echo "$line" | sed -r 's/.*import \"(.*)\"$/\1/')
   pdzName=$(echo "$pdzName" | sed -r 's/(.*).lua$/\1/') || "$pdzName" # strip .lua if found
-  # echo "pdzName $pdzName"
+  echo "pdzName $pdzName"
   pdzPath="$invokeDir/$pdxPath/$matchDirPath/$pdzName.pdz"
   # file may not exist when it was deleted in the same run. imports for the same file may be found in multiple source files
   removeIfExists "$pdzPath"
